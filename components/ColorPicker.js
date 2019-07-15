c3e9e5b04895c4798cb8d2329c566672b82fdf6d
Lucas Volle <Lucas@Volle.io>
2019-07-14 23:55:19 -0400

    Open/close color picker on click

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -8,56 +8,93 @@ class ColorPicker extends Component
 	constructor(props) 
 	{
 		super(props);
+
+		this.onClick = this.onClick.bind(this);
 	}
 
 	componentDidMount()
 	{
-		const colorPickerElement = ReactDOM.findDOMNode(this).querySelector(".color-picker");
-		const colourPicker = new ColourPicker(colorPickerElement, (rgba) => 
-			{
-				document.body.style.backgroundColor = `rgb(${rgba.R}, ${rgba.G}, ${rgba.B})`;
+		const container = ReactDOM.findDOMNode(this);
+		const pickerElement = container.querySelector(".color-picker");
+		const indicator = container.querySelector(".color-picker-indicator");
 
-				const whiteTintHsl = colourPicker.GetColour().GetHSL();
-				if (whiteTintHsl.L < 0.3)
-					document.documentElement.classList.add("dark-mode");
-				else
-					document.documentElement.classList.remove("dark-mode");
+		const colourPicker = new ColourPicker(pickerElement, (rgba) => 
+		{
+			document.body.style.backgroundColor = `rgb(${rgba.R}, ${rgba.G}, ${rgba.B})`;
+			indicator.style.backgroundColor = `rgb(${rgba.R}, ${rgba.G}, ${rgba.B})`;			
 
-				whiteTintHsl.S = Math.min(whiteTintHsl.S, whiteTintHsl.L);
-				whiteTintHsl.L = 0.9 + whiteTintHsl.L / 10;
-				const whiteTintRgb = new Colour(whiteTintHsl);
-				whiteTintRgb.A = 98;
-				
-				document.querySelector(".header-container").style.backgroundColor = whiteTintRgb.ToCssString(true);
-			});
-  	}
+			const whiteTintHsl = colourPicker.GetColour().GetHSL();
+			if (whiteTintHsl.L < 0.3)
+				document.documentElement.classList.add("dark-mode");
+			else
+				document.documentElement.classList.remove("dark-mode");
+
+			whiteTintHsl.S = Math.min(whiteTintHsl.S, whiteTintHsl.L);
+			whiteTintHsl.L = 0.9 + whiteTintHsl.L / 10;
+			const whiteTintRgb = new Colour(whiteTintHsl);
+			whiteTintRgb.A = 98;
+			
+			document.querySelector(".header-container").style.backgroundColor = whiteTintRgb.ToCssString(true);
+		});
+	}
+	  
+	onClick()
+	{
+		const container = ReactDOM.findDOMNode(this);
+
+		// Hide color picker on click outside
+		const onBodyClick = (evt) =>
+		{
+			if (evt.target.closest(".color-picker"))
+				return;
+
+			document.body.removeEventListener("click", onBodyClick);
+			container.classList.remove("active");				
+		};
+		document.body.addEventListener("click", onBodyClick);
+
+		// Show color picker
+		container.classList.add("active");
+	}
 
 	render()
 	{
 		return (
-			<div className="color-picker-container">
-				<div className="color-picker-border">
+			<div className="color-picker-container no-invert">
+				<div className="color-picker-border" onClick={this.onClick}>
 					<div className="color-picker-indicator"></div>
 				</div>
-				<div className="color-picker no-invert"></div>
+				<div className="color-picker"></div>
 				<style jsx>
 				{`
 					.color-picker-border {
-						width: 16px;
-						height: 16px;
-						border: 1px solid #323232;
+						border: 1px solid #AAA;
 						border-radius: 4px;
 						padding: 4px;
+						background-color: #FFF;
+
+						cursor: pointer;
 					}
+					.color-picker-container.active .color-picker-border {
+						border: 1px solid #323232;
+					}
+
 					.color-picker-indicator {
-						background-color: white;
+						width: 16px;
+						height: 16px;
+						background-color: #FFF;
+						border: 1px solid #AAA;
 						border-radius: 2px;
 					}
 
 					.color-picker {
+						display: none;
 						position: absolute;
 						bottom: 48px;
 						transform: translateX(calc(-50% + 16px));
+					}
+					.color-picker-container.active .color-picker {
+						display: block;
 					}
 				`}
 				</style>
